7feb81290a8d5b3396b2994914c10989cadda177
nalex <nalex@DESKTOP-ORVQGTU>
2022-10-09 13:45:22 +0300

    Add project files.

---
+++ Дз1(WinForms)/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Дз1_WinForms_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox1.Text)) {
+                MessageBox.Show("Please enter a number");
+                return;
+            }
+            MessageBox.Show($"You entered number {textBox1.Text}");
+        }
+    }
+}
---
+++ Дз1/Program.cs
@@ -0,0 +1,3 @@
+﻿Console.Write("Enter your number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"You entered number {num}");
